ddb9b59ac0ccf61ca0d467cbf042e966ef868319
Dikeola Ogunmola <49400812+Dikeola@users.noreply.github.com>
2026-06-29 09:46:36 +0100

    Update iris_web_app.py

--- iris_web_app.py
+++ iris_web_app.py
@@ -1,15 +1,26 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
 
-loaded_model = pickle.load(open('iris_prediction.pkl', 'rb'))
+
+iris = load_iris()
+iris_x = pd.DataFrame(iris.data, columns=iris.feature_names)
+iris_y = pd.Series(iris.target)
+
+X_train, X_test, y_train, y_test = train_test_split(iris_x, iris_y, test_size=.2, random_state=1)
+
+log = LogisticRegression(class_weight='balanced')
+log.fit(X_train, y_train)
+
 
 def predictor(input_data):
 	input_data_as_numpy_array = np.asarray(input_data)
 	reshaped_numpy_array = input_data_as_numpy_array.reshape(1,-1)
 	
-	prediction = loaded_model.predict(reshaped_numpy_array)
+	prediction = log.predict(reshaped_numpy_array)
 	return prediction
 	
 def main():
